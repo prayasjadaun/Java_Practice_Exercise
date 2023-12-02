2d85f90d8343ddaf6fe3ef6dc3d47fb84583345b
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-02 21:49:36 +0530

    changes

--- practicesLoops.java
+++ practicesLoops.java
@@ -1,12 +1,42 @@
+// Question-1- Write a program to print a following problem
+// *****
+// ****
+// ***
+// **
+// *
+// Solution--------------------------------------
+
+// public class practicesLoops {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for (int i = n; i > 0; i--) {
+//             for (int j = 0; j < i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+
+// }
+// --------------------------------------------------------------
+
+// Question-2-------------------------------------
+// Write a program to sum first n even numbers using while loop
+// Solution-------------------------------------------------
+
+import java.util.*;
+
 public class practicesLoops {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = n; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.print("\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum = sum + (2*i);
+            System.out.println(sum);
+
         }
     }
-
 }
