c1c1088d208349f688107b09d768038a28f5043a
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-02 20:54:18 +0530

    some changes

--- practicesLoops.java
+++ practicesLoops.java
@@ -1,6 +1,12 @@
 public class practicesLoops {
-    public static void main(String[]args){
-        
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
-    
+
 }
