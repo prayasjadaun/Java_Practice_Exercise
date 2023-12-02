ee953bb49e85f74ad8d43962708951e0a46920e0
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-02 23:57:56 +0530

    updated

--- practicesLoops.java
+++ practicesLoops.java
@@ -24,6 +24,25 @@
 // Write a program to sum first n even numbers using while loop
 // Solution-------------------------------------------------
 
+// import java.util.*;
+
+// public class practicesLoops {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the number: ");
+//         int n = sc.nextInt();
+//         int sum = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             sum = sum + (2*i);
+//             System.out.println(sum);
+//         }
+//     }
+// }
+// -----------------------------------------------------------------------------
+// Question-3-Write a program to print multiplication table of a given number.
+// Solution------------------------------------------------------------------------
+
 import java.util.*;
 
 public class practicesLoops {
@@ -31,11 +50,13 @@ public class practicesLoops {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int n = sc.nextInt();
-        int sum = 0;
 
-        for (int i = 0; i < n; i++) {
-            sum = sum + (2*i);
-            System.out.println(sum);
+        for (int i = 1; i <= 10; i++) {
+            for (int j = n; j <= n; j++) {
+                System.out.println(n + "*" + i + "=" + (i * n));
+            }
+
         }
+
     }
 }
