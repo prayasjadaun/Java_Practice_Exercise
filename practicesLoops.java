68dc6c1166ff1899d293f6ebf7c01ed8ea1f39ad
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-04 20:07:22 +0530

    updated

--- practicesLoops.java
+++ practicesLoops.java
@@ -64,31 +64,89 @@
 // import java.util.*;
 
 // public class practicesLoops {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         System.out.print("Enter the number: ");
-//         int n = sc.nextInt();
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter the number: ");
+//     int n = sc.nextInt();
 
-//         for (int i = 10; i >= 1; i--) {
-//             System.out.println(n + " X " + i + " = " + (i * n));
-//         }
-
+//     for (int i = 10; i >= 1; i--) {
+//         System.out.println(n + " X " + i + " = " + (i * n));
 //     }
+
+// }
 // }.
 // -----------------------------------------------------------------
 // Question-5-Write a program to fint a factorial of a given number using for loop.
 // Solution---------------------------------------------------------
 
+// import java.util.*;
+
+// public class practicesLoops {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the number: ");
+//         int n = sc.nextInt();
+//         int fact = 1;
+//         for (int i = fact; i <= n; i++) {
+//             fact *= i;
+//         }
+//         System.out.println(fact);
+//     }
+// }
+// ---------------------------------------------------------------------
+// Question-6- Repeat 5 usong while loop
+// Solution------------------------------
+
+// import java.util.*;
+
+// public class practicesLoops{
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the number: ");
+//         int n = sc.nextInt();
+//         int fact = 1;
+//         int i = 1;
+//         while(i<= n){
+//             fact*=i;
+//             i++;
+//         }System.out.println(fact);
+//     }
+// }
+// -------------------------------------------------------------------------
+// Question-7-Write a program to print a following problem using while loop
+// *****
+// ****
+// ***
+// **
+// *
+// Solution-------------------------------------
 import java.util.*;
 
-public class practicesLoops{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int n = sc.nextInt();
-        int fact = 1;
-        for(int i = fact;i<=n;i++){
-            System.out.println(fact*=i);
+public class practicesLoops {
+    // public static void main(String[]args){
+    // Scanner sc = new Scanner(System.in);
+    // System.out.print("Enter the number: ");
+    // int n = sc.nextInt();
+    // int i =1;
+    // while(i>=n){
+    // while(j<0)
+
+    // i--;
+    // }
+
+    // }
+
+    public static void main(String[] args) {
+        int n = 5; // Define the number of rows
+        int i = 0;
+        while (i < n) {
+            int j = n - i;
+            while (j > 0) {
+                System.out.print("*");
+                j--;
+            }
+            System.out.println();
+            i++;
         }
     }
 }
