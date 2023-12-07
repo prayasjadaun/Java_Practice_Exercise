08037b37ec0b74dfb3e91d689ba10693f02a0c75
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-07 15:52:44 +0530

    updated

---
+++ array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class array {
+    public static void main(String[]args){
+        int [] marks = {98,89,80,87,99};
+        for(int i = marks.length - 1; i>= 0; i--){
+            System.out.println(marks[i]);
+
+        }
+
+    }
+}
--- practicesLoops.java
+++ practicesLoops.java
@@ -189,20 +189,23 @@
 // Question-10- Write a program to find the value of one number raised to the power of another
 // Solution--------------------------------------------------------
 
-import java.util.*;
+// import java.util.*;
 
-public class practicesLoops {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the base number: ");
-        int base = sc.nextInt();
-        System.out.print("Enter the power number");
-        int power = sc.nextInt();
+// public class practicesLoops {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the base number: ");
+//         int base = sc.nextInt();
+//         System.out.print("Enter the power number");
+//         int power = sc.nextInt();
 
-        int result = 1;
-        for (int i = 1; i <= power; i++) {
-            result *= base;
-        }
-        System.out.println("Result: " + result);
-    }
-}
+//         int result = 1;
+//         for (int i = 1; i <= power; i++) {
+//             result *= base;
+//         }
+//         System.out.println("Result: " + result);
+//     }
+// }
+
+// -----------------------------------------------------------------------
+// Question-11-
