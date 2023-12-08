9ba7ea1dae18f8c9d3a0e5f2b4a847697c3d62c7
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-08 19:45:19 +0530

    updated

--- practiceArray.java
+++ practiceArray.java
@@ -15,12 +15,34 @@
 // -------------------------------------------------------------------
 // Question-2-Write a program to Sort Numeric Array in Ascending Order.
 // Solution-------------------------------
+// import java.util.*;
+// public class practiceArray{
+//     public static void main(String[]args){
+//         int [] arr = {23,97,56,34,134,87,56,76};
+//         System.out.println("Oringin Array: "+ Arrays.toString(arr));
+//         Arrays.sort(arr);
+//         System.out.println("Sorted Array: "+Arrays.toString(arr));
+//     }
+// }
+// ------------------------------------------------------------------------
+// Question-3--
+// Solution-------------------------
 import java.util.*;
-public class practiceArray{
-    public static void main(String[]args){
-        int [] arr = {23,97,56,34,134,87,56,76};
-        System.out.println("Oringin Array: "+ Arrays.toString(arr));
-        Arrays.sort(arr);
-        System.out.println("Sorted Array: "+Arrays.toString(arr));
+
+public class practiceArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int l = arr.length;
+        int n = Math.floorDiv(l, 2);
+        int temp;
+        for (int i = 0; i < n; i++) {
+            temp = arr[i];
+            arr[i] = arr[l - 1 - i];
+            arr[l - 1 - i] = temp;
+
+        }
+        for (int element : arr) {
+            System.out.print(element+" ");
+        }
     }
 }
