53b8e7445404b1abc862e9a3ef2202811c1d4675
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-17 14:38:09 +0530

    cmt

--- Conditions.java
+++ Conditions.java
@@ -39,7 +39,7 @@
 //         }
 //     }
 // }
-public class  {
+public class Conditions  {
     public static int search(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
--- tempCodeRunnerFile.java
+++
@@ -1,2 +0,0 @@
-int[] nums = { 2, 7, 11, 13 };
-        // int target = 9;
