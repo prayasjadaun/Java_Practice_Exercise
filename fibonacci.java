70e0232c890454b92109e50d0fb943d5a519ffcc
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2023-12-22 20:26:25 +0530

    print recursion pattern

--- fibonacci.java
+++ fibonacci.java
@@ -1,20 +1,21 @@
 /*
 QUestion----------------------------------------------------
  Write a prgram to print n term of fibonacci series using recursion.
- */
-public class fibonacci {
-    public static int fib(int n) {
-        // Fibonacci Series--0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
-        if (n == 1 || n == 2) {
-            return n - 1;
-        } else {
-            return fib(n - 1) + fib(n - 2);
+ public class fibonacci {
+     static int fib(int n) {
+         // Fibonacci Series--0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+         if (n == 1 || n == 2) {
+             return n - 1;
+            } else {
+                return fib(n - 1) + fib(n - 2);
+            }
         }
+        
+        public static void main(String[] args) {
+            int result = fib(8);
+            System.out.println(result);
+        }
+        
     }
-
-    public static void main(String[] args) {
-        int result = fib(8);
-        System.out.println(result);
-    }
-
-}
+    ------------------------------------------------------
+    */
---
+++ practicesrec.java
@@ -0,0 +1,19 @@
+/*
+ Question-------------------------------------
+
+ */
+public class practicesrec {
+    static int pattern_recursion(int n){
+        if(n>0){
+            pattern_recursion(n-1);
+            for(int i =0; i<n; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        return n;
+    }
+    public static void main(String[]args){
+        pattern_recursion(5);
+    }
+}
