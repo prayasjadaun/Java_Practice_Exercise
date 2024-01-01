be9e97178edc2f6603fe7a43ee7b30eae44b460b
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-01 22:41:23 +0530

    Solved Rectangle Problem

--- OOps.java
+++ OOps.java
@@ -1,17 +1,19 @@
-
+//Problem 1------------------------------------------
+/*
+ 
 class Employee {
     int id;
     int salary;
     String name;
-
+    
     public void EmployeeDetails() {
         System.out.println("My Name is " + name);
         System.out.println("My Id is " + id);
     }
-
+    
     public int getSalary() {
         return salary;
-
+        
     }
 }
 
@@ -19,12 +21,12 @@ public class OOps {
     public static void main(String[] args) {
         Employee Prayas = new Employee();
         Employee Krishna = new Employee();
-
+        
         Prayas.name = "Prayas Jadaun";
         Prayas.id = 25;
         Prayas.salary = 1200000;
         Prayas.EmployeeDetails();
-
+        
         int salary = Prayas.getSalary();
         System.out.println("My Average salary "+salary);
         
@@ -32,6 +34,53 @@ public class OOps {
         Krishna.id = 48;
         Krishna.salary = 1000000;
         Krishna.EmployeeDetails();
+        
+    }
+}
+//Problem----------------------------------------------------------
 
+class Square {
+    int side;
+
+    public int area() {
+        return side * side;
+    }
+    
+    public int perimeter() {
+        return 4 * side;
+    }
+}
+
+public class OOps {
+    public static void main(String[] args) {
+        Square sq = new Square();
+        sq.side = 3;
+        System.out.println(sq.area());
+        System.out.println(sq.perimeter());
+    }
+}
+*/
+//Problem-----------------------------------------
+public class OOps {
+    class Rectangle {
+        int length;
+        int width;
+
+        public int area() {
+            return width * length;
+        }
+        public int perimeter() {
+            return 2 * (length + width);
+        }
+    }
+    public static void main(String[] args) {
+        OOps oops = new OOps();
+        OOps.Rectangle rc = oops.new Rectangle();
+
+        rc.length = 3;
+        rc.width = 2;
+
+        System.out.println(rc.area());
+        System.out.println(rc.perimeter());
     }
 }
