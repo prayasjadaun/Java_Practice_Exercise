303cc1fb46f7f19e5bf238f3c8ecd1cb7cc16635
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-04 19:39:50 +0530

    Constructor OverLoading

--- Constructor.java
+++ Constructor.java
@@ -2,24 +2,27 @@
 class MyNewEmployee {
     private int id;
     private String name;
+    private int salary;
 
-    public MyNewEmployee(String myname, int myId) {
+    public MyNewEmployee(String myname, int myId, int mysalary) {
         id = myId;
         name = myname;
+        salary = mysalary;
     }
     public String getName() { return name;}
     public void setName(String n) { this.name = n;}
-    public void setId(int i) { this.id = i;}
     public int getId() { return id; }
+    public void setId(int i) { this.id = i;}
+    public int getSalary(){ return salary;}
+    public void setSalary(int salary){ this.salary = salary;}
 }
 
 public class Constructor {
     public static void main(String[] args) {
 
-        MyNewEmployee Prayas = new MyNewEmployee("Prayas Jadaun",25);
-        // Prayas.setName("Prayas Jadaun");
+        MyNewEmployee Prayas = new MyNewEmployee("Prayas Jadaun",25,10000000);
         System.out.println(Prayas.getName());
-        // Prayas.setId(25);
         System.out.println(Prayas.getId());
+        System.out.println(Prayas.getSalary());
     }
 }
