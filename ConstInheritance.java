f807c4fb01d81ea3323ecc391948b9d7ca807456
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-06 23:28:28 +0530

    Constructor inheritance practice

--- ConstInheritance.java
+++ ConstInheritance.java
@@ -2,11 +2,11 @@
 class Base {
 
     Base() {
-        System.out.println("I am base const");
+        System.out.println("I am Base const");
     }
 
     Base(int x) {
-        System.out.println("I am base const with value of x as " + x);
+        System.out.println("I am overload constructor with value of x as " + x);
     }
 
 }
@@ -19,17 +19,28 @@ class Derived extends Base {
 
     Derived(int x, int y) {
         super(x);
-        System.out.println("I am Deived Const with vaouse of y as " + y);
+        System.out.println("I am overload Deived Constructor with value of y as " + y);
     }
 
+}
+
+class ChildOfDerived extends Derived {
+    ChildOfDerived() {
+        System.out.println("I am Child of derived  constructor ");
+    }
+
+    ChildOfDerived(int x, int y, int z) {
+        super(x, y);
+        System.out.println("I am overload Child of derived constructor with value of z as " + z);
+    }
 }
 
 public class ConstInheritance {
     public static void main(String[] args) {
         // Base b = new Base();
-        Derived d = new Derived(14, 15);
-        // System.out.println(b.getX());
-        // System.out.println(d.getY());
+        // Derived d = new Derived(14, 15);
+        // ChildOfDerived cd = new ChildOfDerived();
+        ChildOfDerived cd1 = new ChildOfDerived(10, 20, 30);
 
     }
 }
