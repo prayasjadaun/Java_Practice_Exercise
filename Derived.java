771ec26e53115a4caf31be50a33b89dc8c245068
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-06 20:26:37 +0530

    Inheritance In constructors

---
+++ Derived.java
@@ -0,0 +1,11 @@
+
+public class Derived {
+
+    public char[] getY() {
+        return null;
+    }
+
+    public void setY(int i) {
+    }
+
+}
---
+++ Inheritance.java
@@ -0,0 +1,34 @@
+class Base {
+    public int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    class Derived extends Base {
+        public int y;
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Base b = new Base();
+        b.setX(3);
+        System.out.println(b.getX());
+        Base.Derived d = new Base().new Derived(); // Corrected instantiation
+        d.setY(2);
+        System.out.println(d.getY());
+    }
+}
