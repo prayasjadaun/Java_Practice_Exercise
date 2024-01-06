4d9287b1e4e4bdaf9f62c559e14db1cb232ea36d
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-07 01:32:48 +0530

    Questions solve in inheritance in java

---
+++ PracticeInheritance01.java
@@ -0,0 +1,36 @@
+
+class Circle {
+    public int radius;
+
+    Circle(int r) {
+        System.out.println("I am circle constructor");
+        this.radius = r;
+    }
+
+    public double Area() {
+        return Math.PI * radius * radius;
+    }
+
+}
+
+class Cylinder extends Circle {
+    public int height;
+
+    Cylinder(int r, int h) {
+        super(r);
+        this.height = h;
+    }
+
+    public double Volume() {
+        return Math.PI * radius * radius * height;
+    }
+}
+
+public class PracticeInheritance01 {
+    public static void main(String[] args) {
+        Circle cr = new Circle(3);
+        System.out.println(cr.Area());
+        Cylinder cl = new Cylinder(2, 4);
+        System.out.println(cl.Volume());
+    }
+}
