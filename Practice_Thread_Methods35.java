0cd09d819eb7f42bb389170a1a5bdf9cc5a8a93e
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-19 23:23:51 +0530

    Priority in java thread method

--- Practice_Thread_Methods35.java
+++ Practice_Thread_Methods35.java
@@ -1,6 +1,6 @@
 class Method_thread1 extends Thread {
     public void run() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Good Morning Developers");
             try {
                 Thread.sleep(200);
@@ -29,7 +29,11 @@ public class Practice_Thread_Methods35 {
         Method_thread1 mt1 = new Method_thread1();
         Method_thread2 mt2 = new Method_thread2();
 
-        mt1.start();
-        mt2.start();
+        // mt1.setPriority(4);
+        // mt2.setPriority(3);
+        System.out.println(mt1.getPriority());
+        System.out.println(mt2.getPriority());
+        // mt1.start();
+        // mt2.start();
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+mt1.setPriority(4);
+        // mt2.setPriority(3);
