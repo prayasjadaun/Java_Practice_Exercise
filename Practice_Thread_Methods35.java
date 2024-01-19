8ed11c12b67fda9855db0ecd9867cdf1cdbb489e
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-19 23:18:48 +0530

    Try catch method in java

---
+++ Practice_Thread_Methods35.java
@@ -0,0 +1,35 @@
+class Method_thread1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Good Morning Developers");
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class Method_thread2 extends Thread {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Welcome back to my Youtube channel");
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class Practice_Thread_Methods35 {
+    public static void main(String[] args) {
+        Method_thread1 mt1 = new Method_thread1();
+        Method_thread2 mt2 = new Method_thread2();
+
+        mt1.start();
+        mt2.start();
+    }
+}
