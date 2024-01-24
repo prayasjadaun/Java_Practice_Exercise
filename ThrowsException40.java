5ef9932b15b031f3cb74aec3f1f3f8fe102639e3
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-24 22:09:15 +0530

    Throws keyword in java

---
+++ ThrowsException40.java
@@ -0,0 +1,19 @@
+
+
+public class ThrowsException40 {
+    public static int divide(int a, int b) throws ArithmeticException {
+        int result = a / b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        try{
+            int c = divide(5, 0);
+            System.out.println(c);
+        }
+        catch(Exception e ){
+            System.out.println("Throws an Exception");
+        }
+    }
+}
--- tempCodeRunnerFile.java
+++
@@ -1,2 +0,0 @@
-mt1.setPriority(4);
-        // mt2.setPriority(3);
