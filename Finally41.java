88c332078e143140bc46788e38dba14914b31afa
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-24 22:13:25 +0530

    Finally keyword use in java

---
+++ Finally41.java
@@ -0,0 +1,22 @@
+public class Finally41 {
+    public static int greet(){
+        try{
+
+            int a = 10;
+            int b = 2;
+            int c = a/b;
+            return c;
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        finally{
+            System.out.println("This is the end of this function....");
+        }
+        return 0;
+    }
+    public static void main(String[]args){
+        int k = greet();
+        System.out.println(k);
+    }
+}
